3b253f398ba8e00e9165da154dda49a9a9dd6130
luis1gonzalez <luis1gonzalez@hotmail.com>
2024-02-08 00:01:10 +0100

    mejorando aboutme y projects

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,9 +9,9 @@ const AboutMe = () => {
     <div className="flex flex-col lg:flex-row gap-5 md:gap-0 p-5 justify-center items-center border border-gray-700/50">
         <p className="text-2xl md:text-3xl md:w-[25%] md:mb-4 text-center">{lang === 'english' ? 'About Me' : 'Sobre Mi'}</p>
         <p className="flex text-lg md:w-[75%] text-justify justify-center">{lang === 'english' ? (
-            'I am Industrial Maintenance Engineer with more than 14 years of work experience in the area of Occupational Risk Prevention and in the execution of projects of various kinds. I consider myself passionate about technology in general, but especially for the IT world.'
+            'I am Industrial Maintenance Engineer with work experience in the area of Occupational Risk Prevention and in the execution of projects of various kinds. I consider myself passionate about technology in general, but especially for the IT world.'
         ) : (
-            'Soy Ingeniero en Mantenimiento Industrial con más de 14 años de experiencia laboral en el área de Prevención de Riesgos Laborales y en ejecución de proyectos de diversa índole. Me considero un apasionado de la tecnología en general, pero sobre todo por el mundo IT.'
+            'Soy Ingeniero en Mantenimiento Industrial con experiencia en el área de Prevención de Riesgos Laborales y en ejecución de proyectos de diversa índole. Me considero un apasionado de la tecnología en general, pero sobre todo por el mundo IT.'
         )}</p>
     </div>
   )
--- src/pages/ProjectsList.jsx
+++ src/pages/ProjectsList.jsx
@@ -2,11 +2,23 @@ import NavMain from "../components/NavMain"
 import useLanguage from "../hooks/useLanguage"
 import { detailsProjects } from "../data/projectsData"
 import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 
 const ProjectsDetails = () => {
     const { lang } = useLanguage()
     const navigate = useNavigate()
+    const [filterProjects, setFilterProjects] = useState([])
+    const [filterPractices, setFilterPractices] = useState([])
+
+useEffect(() => {
+    const filterProjectsx = detailsProjects.filter(filter => filter.type === 'project');
+    setFilterProjects(filterProjectsx)
+
+    const filterPracticesx = detailsProjects.filter(filter => filter.type === 'practice');
+    setFilterPractices(filterPracticesx)
+
+},[])
 
     return (
         <div className="flex flex-col min-h-screen items-center w-[100%]">
@@ -16,10 +28,28 @@ const ProjectsDetails = () => {
             <div className='flex flex-col max-w-[1100px] min-w-[320px] w-[95%] h-[100%] p-5 bg-gray-900/50 gap-y-5 md:gap-14'>
 
                 <div className="flex flex-col gap-10 md:gap-0 p-5 justify-center items-center border border-gray-700/50">
-                    <p className="text-2xl md:text-3xl md:w-[25%] md:mb-4 text-center">{lang === 'english' ? 'Projects' : 'Proyectos'}</p>
 
                     <div className="flex flex-col items-center gap-3 box-border w-[250px] sm:w-[300px] p-3 lg:flex-row lg:justify-center lg:w-[100%] flex-wrap h-auto">
-                        {detailsProjects.map(details => (
+                    <p className="text-2xl md:text-3xl md:w-[25%] mb-3 md:mb-4 text-center">{lang === 'english' ? 'Projects' : 'Proyectos'}</p>
+                        {filterProjects.map(details => (
+                            <div
+                                className="borderx flex flex-col items-center justify-center rounded-xl shadow-md shadow-slate-50/50 h-[180px] gap-2 p-1 sm:p-4  w-[100%] lg:w-[30%] hover:bg-sky-500/10"
+                                key={details.id}
+                                onClick={() => navigate(`/project/${details.name}`)}
+                            >
+                                {/* bg-sky-500/10 */}
+                                <p className="text-xl text-center h-[20%]">{details.name}</p>
+                                <img className="w-[70%] h-[70%]"
+                                    src={details.image}
+                                    alt={`logo del proyecto ${details.name}`} />
+                            </div>
+
+                        ))}
+                    </div>
+
+                    <div className="flex flex-col items-center gap-3 box-border w-[250px] sm:w-[300px] p-3 lg:flex-row lg:justify-center lg:w-[100%] flex-wrap h-auto">
+                    <p className="text-2xl md:text-3xl md:w-[25%] mb-3 md:mb-4 text-center">{lang === 'english' ? 'Practices' : 'Practicas'}</p>
+                        {filterPractices.map(details => (
                             <div
                                 className="borderx flex flex-col items-center justify-center rounded-xl shadow-md shadow-slate-50/50 h-[180px] gap-2 p-1 sm:p-4  w-[100%] lg:w-[30%] hover:bg-sky-500/10"
                                 key={details.id}
